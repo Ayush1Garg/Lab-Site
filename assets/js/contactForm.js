9bf875afb762f024aa0d9be54a2dab30cb7d5fba
Ayush Garg <ayushgarg1503@gmail.com>
2024-07-23 20:00:08 +0530

    conatctForm

---
+++ assets/js/contactForm.js
@@ -0,0 +1,16 @@
+const contactForm = document.querySelector("#contactForm");
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
+    fetch(url, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors"
+    })
+        .then(() => {
+            window.location.href = "/"
+            alert("Message Sent");
+        })
+        .catch((err) => alert(`Error : ${err}, Please try again`));
+})
