5d2cb866068679752ed2c7d62cbe8aa13a664b99
Ayush Garg <ayushgarg1503@gmail.com>
2024-07-24 15:27:02 +0530

    changed/added a form

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -3,6 +3,9 @@ contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const url = e.target.action;
     const formData = new FormData(contactForm);
+    // console.log(formData);
+    localStorage.setItem("formData", formData);
+
     fetch(url, {
         method: "POST",
         body: formData,
